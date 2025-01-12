a3d994c8a69e483fb111340d8b184cee7345c8d2
anolifernando <anolifernando@gmail.com>
2025-01-13 00:36:08 +0100

    Skrivit alla const som hämtar referenser till HTML-element som ska användas senare i koden

---
+++ index.js
@@ -0,0 +1,7 @@
+const titleHead = document.querySelector("title");
+const cityTitle = document.querySelector("h2");
+const cityUndertitle = document.querySelector("h3");
+const cityBoxes = document.querySelector("#cities");
+const distanceTable = document.querySelector("#table");
+const theClosestCity = document.querySelector("#closest");
+const theFurthestCity = document.querySelector("#furthest");
